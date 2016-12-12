3261f66c1d302f0d5249aa8b4c6513362909bb0f
Tintin Ho <holoktin97@gmail.com>
2016-12-13 00:25:08 +0800

    Consider depth and position in the heuristic rate function

--- src/strategy.js
+++ src/strategy.js
@@ -17,7 +17,7 @@ function deferPs () {
     });
 }
 
-function rate (game, player) {
+function originalRate (game, player, depth) {
     if (game.result === undefined) {
         return _.sum(kono.mapPoints(p => {
             const tile = game.at(p);
@@ -33,6 +33,36 @@ function rate (game, player) {
         return 8;
     } else {
         return -8;
+    }
+}
+
+let gridWeight;
+{
+    let weight = [[0.4, 0.8, 0.8, 0.4],
+                  [0.8, 1.0, 1.0, 0.8],
+                  [0.8, 1.0, 1.0, 0.8],
+                  [0.4, 0.8, 0.8, 0.4]];
+    gridWeight = function ({x, y}) {
+        return weight[x][y];
+    };
+}
+
+function rate (game, player, depth) {
+    if (game.result === undefined) {
+        return _.sum(kono.mapPoints(p => {
+            const tile = game.at(p);
+            if (tile === 'empty') {
+                return 0;
+            } else if (tile === player) {
+                return 50 * gridWeight(p) + depth;
+            } else {
+                return -50 * gridWeight(p) - depth;
+            }
+        }));
+    } else if (game.result === player) {
+        return 1000 + depth;
+    } else {
+        return -1000 - depth;
     }
 }
 
@@ -71,7 +101,7 @@ const alphabeta = exports.alphabeta = function (game, player, depth, a, b) {
 
 const minimax = exports.minimax = function (game, player, maxDepth) {
     if (maxDepth === 0 || game.result) {
-        return Promise.resolve(rate(game, player));
+        return Promise.resolve(rate(game, player, maxDepth));
     } else {
         let childrenRatings = _.map(game.childrenState(), state => {
             return deferPs().then(() => minimax(state(), player, maxDepth - 1));
@@ -93,9 +123,13 @@ exports.minimaxOptimal = function (game, depth) {
 };
 
 exports.alphabetaOptimal = function (game, depth) {
+    return exports.alphabetaOptimalOptions(game, depth)
+        .then(pairs => _.maxBy(pairs, 'rating'));
+};
+
+exports.alphabetaOptimalOptions = function (game, depth) {
     return Promise.all(_.map(game.listActions(), action => {
         return alphabeta(game.nextRound(action), game.current, depth - 1, -Infinity, Infinity)
             .then(rating => ({action, rating}));
-    }))
-    .then(pairs => _.maxBy(pairs, 'rating'));
+    }));
 };
--- test/strategy.test.js
+++ test/strategy.test.js
@@ -5,7 +5,7 @@ const strategy = require('../src/strategy');
 
 
 describe('minimax algorithm', function () {
-    this.timeout(5000);
+    this.timeout(7500);
 
     it('know how to win #1', function () {
         let game = kono({
@@ -21,7 +21,7 @@ describe('minimax algorithm', function () {
 
         return strategy.minimaxOptimal(game, 1).then(strategy => {
             expect(strategy.action).to.be.eql({from: {x: 0, y: 1}, to: {x: 3, y: 1}});
-            expect(strategy.rating).to.be.equal(8);
+            expect(strategy.rating).to.be.equal(1000);
         });
     });
 
@@ -39,7 +39,7 @@ describe('minimax algorithm', function () {
 
         return strategy.minimaxOptimal(game, 3).then(strategy => {
             expect(strategy.action).to.be.eql({from: {x: 0, y: 3}, to: {x: 0, y: 2}});
-            expect(strategy.rating).to.be.equal(8);
+            expect(strategy.rating).to.be.equal(1000);
         });
     });
 
@@ -57,7 +57,7 @@ describe('minimax algorithm', function () {
 
         return strategy.alphabetaOptimal(game, 1).then(strategy => {
             expect(strategy.action).to.be.eql({from: {x: 0, y: 1}, to: {x: 3, y: 1}});
-            expect(strategy.rating).to.be.equal(8);
+            expect(strategy.rating).to.be.equal(1000);
         });
     });
 
@@ -75,7 +75,37 @@ describe('minimax algorithm', function () {
 
         return strategy.alphabetaOptimal(game, 3).then(strategy => {
             expect(strategy.action).to.be.eql({from: {x: 0, y: 3}, to: {x: 0, y: 2}});
-            expect(strategy.rating).to.be.equal(8);
+            expect(strategy.rating).to.be.equal(1000);
+        });
+    });
+
+    it('alphabeta: take the shortest path to win', function () {
+        let game = kono({
+            result: undefined,
+            current: 'red',
+            _board: [
+                ['red', 'blue', 'red', 'red'],
+                ['red', 'red', 'empty', 'empty'],
+                ['empty', 'empty', 'empty', 'empty'],
+                ['empty', 'blue', 'empty', 'empty']
+            ]
+        });
+
+        return strategy.alphabetaOptimal(game, 7).then(strategy => {
+            expect(strategy.action).to.be.eql({from: {x: 0, y: 3}, to: {x: 0, y: 1}});
+        });
+    });
+
+    it('alphabeta: kill the enemy as quick as possible', function () {
+        let game = kono({
+            result: undefined,
+            current: 'red',
+            _board: [
+                ['red', 'empty', 'empty', 'red'],
+                ['red', 'empty', 'red', 'empty'],
+                ['empty', 'blue', 'empty', 'empty'],
+                ['blue', 'empty', 'blue', 'empty']
+            ]
         });
     });
 
@@ -84,7 +114,7 @@ describe('minimax algorithm', function () {
         game.act({from: {x: 2, y: 0}, to: {x: 2, y: 2}});
 
         return strategy.alphabetaOptimal(game, 7).then(strategy => {
-            expect(strategy.action).to.be.eql({from: {x: 1, y: 3}, to: {x: 1, y: 1}});
+            expect(strategy.action).to.be.eql({from: {x: 0, y: 2}, to: {x: 2, y: 2}});
             game.act(strategy.action);
         })
         .then(() => {
@@ -92,7 +122,7 @@ describe('minimax algorithm', function () {
             return strategy.alphabetaOptimal(game, 7);
         })
         .then(strategy => {
-            expect(strategy.action).to.be.eql({from: {x: 0, y: 3}, to: {x: 0, y: 1}});
+            expect(strategy.action).to.be.eql({from: {x: 1, y: 2}, to: {x: 3, y: 2}});
         });
     });
 });
